8ff877340bdb3b3e4089fd796eecc9bd78a63877
lcayment <lucie.cayment@gmail.com>
2021-05-20 17:52:29 +0200

    add données

--- donnees.php
+++ donnees.php
@@ -70,8 +70,27 @@
                 <div class="col-md-12 margin_bottom">
                     <div class="row">
                         <div class="Games">
-                            <h3>Les observations</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <p>Ces données ont été récoltés via les observations concentrées dans un journal de bord ainsi que grâce aux grilles d'observations.
+                            </p>
+                            <p>L’analyse que nous allons porter ici est entièrement basée sur des données récoltées grâce à nos observations lors des séances dédiées à la robotique. Etant donné que nous ne sommes pas arrivées au terme de l’application d’auto-évaluation, nous ne bénéficions pas de données quantifiables. </p>
+                            <p> L’observation est, dans le cadre de notre projet, une méthode très pertinente pour la récolte de données car elle nous permet de recueillir des informations objectives et diverses concernant à la fois l’ambiance générale, le comportement des élèves avec le robot, entre eux et avec la maîtresse. Cependant, l’absence de données quantitatives et le faible nombre d’observations rendent la qualité de notre analyse moins rigoureuse. </p>
+                            <p> Les observations que nous avons menées nous ont permis de dresser différents éléments influençant le plaisir et la difficulté ressenties par les élèves et donc implicitement leur persévérance au sein d’une activité. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 margin_bottom">
+                    <div class="row">
+                        <div class="Games">
+                            <h3>Le plaisir</h3>
+                            <p> Nous avons observé qu’avant chaque séance de robotique, les élèves ressentaient de l’excitation, de l’impatience et manifestaient beaucoup d’intérêts pour ce qu’ils apprenaient et manipulaient, en l'occurrence, le robot Bluebot. Nous avons noté que cet enthousiasme et curiosité perduraient durant l’intégralité de la séance et ceux indépendamment de la nature de la séance (découverte ou résolution de problème sous forme de défis). Nous avons aussi noté tout le côté ludique que suscite la manipulation du robot Bluebot. Les élèves conçoivent davantage les activités de robotique comme un jeu les mettant au défi de réussir. Ces séances les obligent à prendre en main le robot et à  manipuler ce qui les rendent actifs et leurs permettent d’appliquer les concepts appris antérieurement. Nous pouvons alors en tirer une première conclusion : solliciter activement l’enfant dans une démarche ludique consistant à leurs apprendre et à les faire appliquer leurs connaissances par la manipulation du robot est une méthode éducative qui suscite chez eux beaucoup de plaisir. </p>
+
+                            <p> Lors des séances, les élèves sont séparés en groupe. Elles s’articulent le plus souvent sous forme de phases consignes/manipulations qui s’enchaînent. Lors de la phase de consignes, la maîtresse revient sur des notions déjà vues ou en cite des nouvelles et demandent aux groupes d’enfants d’accomplir des tâches de découverte ou de défis. Celles-ci sont mises en action lors de la phase de manipulation. Ainsi ces séances reposent sur un travail collectif. Nous avons observé que le travail en équipe incitaient les élèves à s’impliquer dans l’activité. L’enfant est désireux de donner son avis et d’essayer des actions pour atteindre l’objectif final. Il veut montrer ce dont il est capable. Nos dernières séances d’observation nous ont montré que les élèves, ayant acquis une certaine autonomie à l’égard de la manipulation de Bluebot, une fois lancés dans la résolution d’un problème, persistent et échangent entre eux pour trouver des solutions. Ils ont compris l’enjeu du travail de groupe qui leur permet de mettre en commun leurs connaissances et leurs idées dans l’espoir de trouver une solution à leur problème et de parvenir à la résolution du défi. </br>
+                            Ainsi, le travail de groupe les stimule et les pousse à persévérer dans la résolution du problème. </p>
+
+                            <p> Nous avons aussi remarqué l’importance de la reconnaissance de la maîtresse vis-à-vis de l’élève. Ces derniers la réclament continuellement, dès qu’ils terminent une tâche demandée. Cette reconnaissance leur permet d’augmenter leur confiance et leur estime d’eux. Cela est un moteur à leur motivation et à leur engagement dans l'activité. Ils prennent plaisir à continuer et persévèrent dans leur raisonnement. L’encouragement et l’implication de l’éducatrice est donc essentielle à la persévérance. 
+                            </p>    
+
+                            <p>Ainsi, nous pouvons en conclure que <strong>l’aspect ludique, le travail en groupe et la reconnaissance de la maîtresse</strong> sont des facteurs clés influençant le plaisir ressenti par l’élève lors de la séance. 
                             </p>
                         </div>
                     </div>
@@ -79,18 +98,24 @@
                 <div class="col-md-12 margin_bottom">
                     <div class="row">
                         <div class="Games">
-                            <h3>Les focus group</h3>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
+                            <h3>La difficulté </h3>
+                            <p> Nous avons observé que les élèves qui avaient mal compris la consigne ou les nouvelles notions abordées ont naturellement plus de difficultés à réaliser la tâche demandée. Lors des séances de découverte ( les premières séances de robotique), les élèves doivent à la fois s’accaparer de nouveaux concepts et les mettre en application. Cela représente une difficulté pour certains.  Cela semble évident mais il est important de le rappeler. Il est important de bien dissocier l’aspect théorique de l’aspect expérimental. C’est pourquoi des cours en amont ou la verbalisation à haute voix des instructions est essentielle. Un élève qui comprend parfaitement les consignes et comprend les concepts qu’il manipule aura plus de facilité à réaliser les tâches </p>
+
+                            <p> Nous avons aussi remarqué que la façon d’appréhender un problème impacte fortement sa résolution. Les élèves qui travaillent étape par étape de façon linéaire ont beaucoup plus de facilité à résoudre le problème comparé à ceux qui l’imaginent de façon globale. Séquencer le problème en plusieurs parties leur permet de progresser petit à petit et de visualiser concrètement leur avancée. Il s’agit là d’un gage de persévérance. </p>
+
+                            <p> Par ailleurs, là aussi le travail en groupe à son rôle à jouer. En effet, il favorise l’entraide entre les élèves et les aident à affronter la difficulté à plusieurs. </p>
+
+                            <p> Finalement, <strong>la compréhension des consignes et des notions, la façon d’appréhender un problème et le travail de groupe</strong> sont les éléments que nous avons relevés qui influencent sur la difficulté ressentie par l’élève à résoudre un problème. </p>
+
+                            <p>Finalement, nos observations nous ont permis d’aboutir à dresser la liste de quelques facteurs influençant la persévérance de l’élève dans la résolution de problèmes. Il serait désormais intéressant de voir si ses stratégies de résolution sont aussi mises à profit dans des activités plus scolaires.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12 margin_bottom">
+                 <div class="col-md-12 margin_bottom">
                     <div class="row">
                         <div class="Games">
-                            <h3>Les retours de l'application </h3>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                            </p>
+                            <p>Au terme de notre travail, compte-tenu de la situation sanitaire qui nous a encore bien freiné dans nos objectifs, nous n’avons pas récupéré toutes les données que nous avions prévu d’analyser. </br>
+                            En effet, avec le deuxième confinement des écoles, nous n’avons pas pu réaliser les focus group qui nous auraient permis de récolter les avis des élèves sur leur expérience de robotique éducative. La mise en place de notre application dans les classes n’a de même pas eu lieu.  Nous aurions au moins souhaité faire des tests utilisateurs afin de vérifier son utilisabilité et rectifier certains points si nécessaires afin de permettre au prochain groupe transdisciplinaire de rapidement commencer à recueillir des données quantitatives. </p>
                         </div>
                     </div>
                 </div>
